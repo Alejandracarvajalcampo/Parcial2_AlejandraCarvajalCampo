711cdbeb1dc86164b4469d65d34edc83131b488c
AlejandraCarvajalCampo <alejandracarvajal316210@correo.itm.edu.co>
2023-04-01 23:54:29 -0500

    validation is added if the ticket is not in use in the busines logic

--- ConcertDB/ConcertDB/Controllers/TicketsController.cs
+++ ConcertDB/ConcertDB/Controllers/TicketsController.cs
@@ -19,6 +19,9 @@ namespace ConcertDB.Controllers
             _context = context;
         }
 
+      
+
+
         // GET: Tickets
         public async Task<IActionResult> Index()
         {
@@ -45,28 +48,6 @@ namespace ConcertDB.Controllers
             return View(tickets);
         }
 
-        // GET: Tickets/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Tickets/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UseDate,IsUsed,EntranceGate")] Tickets tickets)
-        {
-            if (ModelState.IsValid)
-            {
-                tickets.Id = Guid.NewGuid();
-                _context.Add(tickets);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tickets);
-        }
 
         // GET: Tickets/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
@@ -89,7 +70,7 @@ namespace ConcertDB.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,UseDate,IsUsed,EntranceGate")] Tickets tickets)
+        public async Task<IActionResult> Edit(Guid id, Tickets tickets)
         {
             if (id != tickets.Id)
             {
@@ -100,6 +81,7 @@ namespace ConcertDB.Controllers
             {
                 try
                 {
+                    tickets.IsUsed = true;
                     _context.Update(tickets);
                     await _context.SaveChangesAsync();
                 }
@@ -118,44 +100,6 @@ namespace ConcertDB.Controllers
             }
             return View(tickets);
         }
-
-        // GET: Tickets/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null || _context.Tickets == null)
-            {
-                return NotFound();
-            }
-
-            var tickets = await _context.Tickets
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tickets == null)
-            {
-                return NotFound();
-            }
-
-            return View(tickets);
-        }
-
-        // POST: Tickets/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            if (_context.Tickets == null)
-            {
-                return Problem("Entity set 'DatabaseContext.Tickets'  is null.");
-            }
-            var tickets = await _context.Tickets.FindAsync(id);
-            if (tickets != null)
-            {
-                _context.Tickets.Remove(tickets);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         private bool TicketsExists(Guid id)
         {
           return (_context.Tickets?.Any(e => e.Id == id)).GetValueOrDefault();
