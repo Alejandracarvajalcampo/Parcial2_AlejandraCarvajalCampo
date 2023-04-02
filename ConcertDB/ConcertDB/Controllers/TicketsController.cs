c38a6fd9f416b28fceecca0c8aa810e1c373fe8f
AlejandraCarvajalCampo <alejandracarvajal316210@correo.itm.edu.co>
2023-04-02 15:37:26 -0500

    views that are not needed are removed

--- ConcertDB/ConcertDB/Controllers/TicketsController.cs
+++ ConcertDB/ConcertDB/Controllers/TicketsController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ConcertDB.DAL;
 using ConcertDB.DAL.Entities;
-using Microsoft.IdentityModel.Tokens;
+
 
 namespace ConcertDB.Controllers
 {
